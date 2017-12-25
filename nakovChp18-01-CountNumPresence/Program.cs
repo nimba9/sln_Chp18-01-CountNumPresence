cfd10cf5b6d73f2ee8ee207b6e601304432365e4
vancisin peter <vancisin.peter@gmail.com>
2017-12-25 21:14:40 +0100

    1st version

--- nakovChp18-01-CountNumPresence/Program.cs
+++ nakovChp18-01-CountNumPresence/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace nakovChp18_01_CountNumPresence
 {
-    class Program
+    class CountNumInSeq
     {
         static void Main(string[] args)
         {
+            string inputSeq = Console.ReadLine();
+            string []inputNumbers = inputSeq.Split(new char[] {',', ' '},StringSplitOptions.RemoveEmptyEntries);
+
+            SortedDictionary<int, int> uniqueNum = new SortedDictionary<int, int>();
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                int currNum = Convert.ToInt32(inputNumbers[i]);
+
+                if(uniqueNum.ContainsKey(currNum))
+                {
+                    uniqueNum[currNum]++;
+                }
+
+                else
+                {
+                    uniqueNum.Add(currNum, 1);
+                }
+            }
+
+            foreach(var number in uniqueNum)
+            {
+                Console.WriteLine("{0}->{1}", number.Key, number.Value);
+            }
+
         }
     }
 }
